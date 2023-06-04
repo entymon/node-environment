168a56b4b75d22410d4485b65463750a9821d03b
anymouse_tsh <pawel.olejniczak@tsh.io>
2023-06-04 23:03:29 +0200

    add typescript

--- api-node/src/server.ts
+++ api-node/src/server.ts
@@ -3,9 +3,10 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World 2!')
 })
 
 app.listen(3001, () => {
     console.log("Hello world")
+    console.log("Hello world 2")
 });
